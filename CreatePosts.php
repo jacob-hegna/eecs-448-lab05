1f54d7d7913de3359f8d1d8d1d44b939c4413b32
Jacob Hegna <jacobhegna@gmail.com>
2017-10-24 02:31:40 -0500

    Add CreatePosts.[php/html]
    Let users post on the forum

---
+++ CreatePosts.php
@@ -0,0 +1,22 @@
+<?php
+
+// Password security is !!really important!! for the KU EECS department
+$mysqli = new mysqli('mysql.eecs.ku.edu', 'jhegna', 'P@$$word123', 'jhegna');
+
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+$query = "INSERT INTO tb_posts (content, author_id) VALUES ('" . $_POST['post_body'] . "', '" . $_POST['user_id'] . "');";
+
+if($_POST['user_id'] != '' 
+    && $_POST['post_body'] != ''
+    && $mysqli->query($query)) {
+    echo "Nice post! The database accepted it. Hope you didn't inject anything!";
+} else {
+    echo $mysqli->error;
+    echo "Oops, something went wrong! That username either didn't exist, either field was empty, or something else went horribly wrong. Sorry!";
+}
+
+?>
